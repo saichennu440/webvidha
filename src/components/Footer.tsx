b9dfc7e6c6f3e256c347d53525982176e35fea49
mesaichennu <chennusaiprasad24@gmail.com>
2026-03-10 11:10:38 +0530

    comment removed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,7 +13,7 @@ export default function Footer() {
   };
 
 
-//comment
+
   return (
     <footer className="bg-slate-900 text-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
